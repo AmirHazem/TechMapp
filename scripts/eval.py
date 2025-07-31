bc4b15e2822a71bd1e5b22d720aa3f007bae08b5
Andrea <amir.hazem@gmail.com>
2025-07-31 05:38:29 +0200

    first commit

---
+++ scripts/eval.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+from __future__ import division
+from operator import itemgetter, attrgetter, add
+import numpy as np
+
+
+#token = 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
